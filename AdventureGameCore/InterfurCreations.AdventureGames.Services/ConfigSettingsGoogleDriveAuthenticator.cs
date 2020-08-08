9579aec1e7fb2970a98b2a88097c4756cf50cc1a
Taldrin <max.j.anderson23@gmail.com>
2020-08-08 10:25:29 +0100

    Removed credentials.json from project for google drive authentication. Added new authenticator for retrieving credential json from config.

---
+++ AdventureGameCore/InterfurCreations.AdventureGames.Services/ConfigSettingsGoogleDriveAuthenticator.cs
@@ -0,0 +1,32 @@
+﻿using InterfurCreations.AdventureGames.Configuration;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using InterfurCreations.AdventureGames.Services.Interfaces;
+using System;
+using System.IO;
+using InterfurCreations.AdventureGames.Logging;
+
+namespace InterfurCreations.AdventureGames.Services
+{
+    public class ConfigSettingsGoogleDriveAuthenticator : IGoogleDriveAuthenticator
+    {
+        private static string[] Scopes = { DriveService.Scope.DriveReadonly };
+
+        public DriveService AuthenticateService(IConfigurationService configService)
+        {
+
+            Log.LogMessage("Looking for google drive credentials in configuration...");
+            var credentialJson = configService.GetConfig("GoogleDrive");
+            var credential = GoogleCredential.FromJson(credentialJson).CreateScoped(Scopes);
+
+            // Create Drive API service.
+            var gDriveService = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Adventure Game Draw.IO Parser",
+            });
+            return gDriveService;
+        }
+    }
+}
